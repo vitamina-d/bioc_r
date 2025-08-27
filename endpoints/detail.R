2890f72f74c9b2480b36da7f825d79ebff348aab
veroyols <vero.scholz@gmail.com>
2025-08-27 11:35:05 -0300

    endpoint detail

--- endpoints/detail.R
+++ endpoints/detail.R
@@ -1,51 +1,53 @@
 library(AnnotationDbi)
 library(org.Hs.eg.db)  
-library(GenomicFeatures)
-library(TxDb.Hsapiens.UCSC.hg38.knownGene)
 
-#* detail muestra info de un gen, dado su ENTREZ 
-#* @param entrez id del gen
+#* detail by ENTREZ 
+#* @param entrez:int id del gen
 #* @get /
 #* @tag endpoints
 #* @serializer unboxedJSON 
-function(entrez = "1717") {
+function(entrez) {
 
   start_time <- Sys.time()
 
-  txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
-
-  #entrez <- AnnotationDbi::select(org.Hs.eg.db, keys = symbol, columns = "ENTREZID", keytype = "SYMBOL")$ENTREZID
-  details <- AnnotationDbi::select(org.Hs.eg.db, keys = entrez, columns = c("ENSEMBL", "ENSEMBLPROT", "UNIPROT", "ENTREZID", "GENETYPE", "MAP", "SYMBOL", "GO", "ONTOLOGY"), keytype = "ENTREZID")
-
-  # Obtener rangos, sin filtrar genes que están en ambas cadenas .. granges / listgranges
-  #range_list <- genes(txdb, single.strand.genes.only = FALSE)[entrez]
-  range <- genes(txdb)[entrez]
-  range_df <- as.data.frame(range)
+  details <- tryCatch({
+    AnnotationDbi::select(org.Hs.eg.db, keys = entrez, columns = c("GENETYPE", "SYMBOL", "ALIAS"), keytype = "ENTREZID")
+  }, error = function(e) NULL)
 
   end_time <- Sys.time()
   time <- as.numeric(difftime(end_time, start_time, units = "secs"))
 
 
-  result <- list(
-    status = "success", 
-    time_secs = time,
-    data = list(
-      entrezID = entrez,
-      symbol = unique(details$SYMBOL),
-      type = unique(details$GENETYPE),
-      location = list(
-        citogenetic = unique(details$MAP),
-        strand = as.character(range_df$strand),
-        chr = as.character(range_df$seqnames),
-        start = range_df$start,
-        end = range_df$end,
-        length = range_df$width
-      ),
-      ensembl_id_gene = unique(details$ENSEMBL),
-      ensembl_id_protein = unique(details$ENSEMBLPROT),
-      uniprot_ids = unique(details$UNIPROT)
-      #go = unique(details$go),
-      #ontology = unique(details$ontology)
+  if (is.null(detail)) {
+      result <- list(
+          code = 500,
+          datetime = start_time,
+          time_secs = time,
+          data = list (
+              message = "try catch"
+          )
+      )
+  } else if (length(detail) == 0) {
+      result <- list(
+          code = 404,
+          datetime = start_time,
+          time_secs = time,
+          data = list (
+              message = paste("no se encontraron valores para el entrez ", alias)
+          )
+      )
+  } else {
+    result <- list(
+      code = 200,
+      datetime = start_time,
+      time_secs = time,
+      data = list(
+        entrez = entrez,
+        symbol = unique(details$SYMBOL),
+        genetype = unique(details$GENETYPE),
+        alias = unique(details$ALIAS)
+      )
     )
-  )
-}#GO
+  }
+  return(result)
+}
---
+++ endpoints/detailfulll.R
@@ -0,0 +1,52 @@
+library(AnnotationDbi)
+library(org.Hs.eg.db)  
+library(GenomicFeatures)
+library(TxDb.Hsapiens.UCSC.hg38.knownGene)
+
+#* detail muestra info de un gen, dado su ENTREZ 
+#* @param entrez id del gen
+#* @get /
+#* @tag endpoints
+#* @serializer unboxedJSON 
+function(entrez = "1717") {
+
+  start_time <- Sys.time()
+
+  txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
+
+  #entrez <- AnnotationDbi::select(org.Hs.eg.db, keys = symbol, columns = "ENTREZID", keytype = "SYMBOL")$ENTREZID
+  details <- AnnotationDbi::select(org.Hs.eg.db, keys = entrez, columns = c("ENSEMBL", "ENSEMBLPROT", "UNIPROT", "ENTREZID", "GENETYPE", "MAP", "SYMBOL", "GO", "ONTOLOGY"), keytype = "ENTREZID")
+
+  # Obtener rangos, sin filtrar genes que están en ambas cadenas .. granges / listgranges
+  #range_list <- genes(txdb, single.strand.genes.only = FALSE)[entrez]
+  range <- genes(txdb)[entrez]
+  range_df <- as.data.frame(range)
+
+  end_time <- Sys.time()
+  time <- as.numeric(difftime(end_time, start_time, units = "secs"))
+
+
+  result <- list(
+    code = 200,
+    datetime = start_time,
+    time_secs = time,
+    data = list(
+      entrezID = entrez,
+      symbol = unique(details$SYMBOL),
+      type = unique(details$GENETYPE),
+      location = list(
+        citogenetic = unique(details$MAP),
+        strand = as.character(range_df$strand),
+        chr = as.character(range_df$seqnames),
+        start = range_df$start,
+        end = range_df$end,
+        length = range_df$width
+      ),
+      ensembl_id_gene = unique(details$ENSEMBL),
+      ensembl_id_protein = unique(details$ENSEMBLPROT),
+      uniprot_ids = unique(details$UNIPROT)
+      #go = unique(details$go),
+      #ontology = unique(details$ontology)
+    )
+  )
+}#GO
